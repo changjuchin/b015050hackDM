4e87659bd2a7f1de9343451ebd110d3a676ade7f
tinnywt <tinnywt@gmail.com>
2015-06-20 00:58:09 +0800

    更新說明

--- model/cart.java
+++ model/cart.java
@@ -1,27 +1,33 @@
+package test1;
+
 import java.util.HashMap;
 
 public class Cart {
 
 	private HashMap<String, Integer> cart = new HashMap<String, Integer>();
 	
-	/*
-	 * 新增一商品至購物車中 k = 商品key, num = 商品數量 
+	/**
+	 * 新增一商品至購物車中
+	 * @param k = 商品key
+	 * @param num = 商品數量
 	 */
 	public void Add(String k, int num){
 		cart.put(k, num);
 	}
 	
-	/*
-	 * 取得購物車中所有商品的 key 跟數量
+	/**
+	 * 取得購物車中所有商品的key跟數量
+	 * @return cart
 	 */
 	public HashMap<String, Integer> GetCart(){
 		return cart;
 	}
 	
-	/*
-	 * 移除一商品
+	/**
+	 * 移除購物車中的一商品
+	 * @param k = 商品key
 	 */
-	public void remove(String k){
+	public void Remove(String k){
 		cart.remove(k);
 	}
 	
